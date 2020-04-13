892c68e034005f3f4241f23e02c72c69899e415b
Amenchrist <amenphronesis@gmail.com>
2020-04-13 23:45:50 +0100

    more changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 
-express().use(cors());
+
+const app = express();
+
+app.use(cors());
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
