a412b08222212e0723f047ca07a069220315b512
Amenchrist <amenphronesis@gmail.com>
2020-04-13 21:26:34 +0100

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors');
+
+express().use(cors());
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
