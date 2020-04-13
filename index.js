10d27446d85bf4e5e4497e4bfbc2c8b518d396ee
Amenchrist <amenphronesis@gmail.com>
2020-04-14 00:28:03 +0100

    taking 1st version of working server live

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const app = express();
 
 app.use(cors());
 
+const user = {
+  name: "amenchrist",
+  class: "developer",
+  level: 1
+}
+app.get('/',(req, res) => res.send(user));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('HALLELUJAHHHHHHHHH!!!'))
+  .get('/', (req, res) => res.send(user))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
 
   //.get('/', (req, res) => res.render('pages/index'))
